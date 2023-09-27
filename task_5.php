387df3fb1e3ba1496e1c7b40b4d3f17171412c97
Monir-GIS <146133027+Monir-GIS@users.noreply.github.com>
2023-09-27 18:53:36 +0600

    Update task_5.php

--- task_5.php
+++ task_5.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Task 5: Password Generator => From Faysal Vai
+Task 5: Password Generator
 Create a PHP function called generatePassword($length) that generates a random password of the specified length.
 The password should include lowerCase letters, upperCase letters, numbers, and special characters (!@#$%^&*()_+).
 Write a PHP program to generate a password with a length of 12 characters using this function and print the password.
